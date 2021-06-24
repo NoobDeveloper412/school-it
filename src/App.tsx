e8eeeafa7e6bc9a28a015ee6218bfb37d30ee264
jokurale <podsiadly.michal@protonmail.com>
2021-06-24 17:39:45 +0200

    Add initial routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+
+import DashboardPage from "@pages/Dashboard.page";
+import IndexPage from "@pages/Index.page";
+import LoginPage from "@pages/Login.page";
 import React from "react";
 
-function App() {
-  return <div className="App">School it</div>;
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/dashboard/:tab?">
+          <DashboardPage />
+        </Route>
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+        <Route path="/" exact>
+          <IndexPage />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
-
-export default App;
