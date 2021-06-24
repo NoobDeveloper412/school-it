68be8874c488d392190bcdee023055e184aa6968
jokurale <podsiadly.michal@protonmail.com>
2021-06-24 17:42:17 +0200

    Change generic reducer to named reducer

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,5 +1,6 @@
+import LoginReducer from "@reducers/Login.reducer";
 import { combineReducers } from "redux";
 
-const root = combineReducers([]);
+const root = combineReducers({ session: LoginReducer });
 
 export default root;
