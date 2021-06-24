554b71c12bb1302b7f98cbb1d1fbc669b2c3a839
jokurale <podsiadly.michal@protonmail.com>
2021-06-24 17:39:34 +0200

    Add URLs config

---
+++ src/config/urls.ts
@@ -0,0 +1,5 @@
+export const AUTH_SERVER_URL = "http://localhost:9000";
+export const AUTH_SERVER_LOGIN_PATH = "/login";
+export const AUTH_SERVER_REFRESH_PATH = "/register";
+
+export const RESOURCE_SERVER_URL = "http://localhost:5000";
