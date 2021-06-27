95951b2f71cb6deffae36934eab3a084d430a5b9
jokurale <podsiadly.michal@protonmail.com>
2021-06-27 19:36:14 +0200

    Implement store persist logic

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,23 @@
+import store, { persistor } from "@redux/store";
+
 import App from "./App";
 import GlobalStyling from "@stylings/Global.styling";
+import { PersistGate } from "redux-persist/integration/react";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider as StoreProvider } from "react-redux";
 import { ThemeProvider } from "styled-components";
-import store from "@redux/store";
 import theme from "@themes/Main.theme";
 
 ReactDOM.render(
   <React.StrictMode>
     <StoreProvider store={store}>
-      <GlobalStyling />
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <GlobalStyling />
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </PersistGate>
     </StoreProvider>
   </React.StrictMode>,
   document.getElementById("root")
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,29 @@
+import { persistReducer, persistStore } from "redux-persist";
+
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "@slices/index";
+import storage from "redux-persist/lib/storage"; // Use localStorage as persist storage
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+/**
+ * Inject store persistor into already present reducers(registered slices)
+ */
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
   devTools: process.env.NODE_ENV !== "production",
 });
 
+/**
+ * Attachable store persistor that will be linked to app's <PersistGate />
+ */
+const persistor = persistStore(store);
+
+export { persistor };
 export default store;
