f5a51aa825a311f33acb2a0d5dfca2af1cace0eb
jokurale <podsiadly.michal@protonmail.com>
2021-06-27 22:53:57 +0200

    Prep initial constant /dashboard subpaths

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,3 +1,16 @@
-const constants = Object.freeze({});
+const constants = Object.freeze({
+  // TODO: Add valid sub paths list
+  dashboardAllowedSubPaths: [
+    "summary",
+    "profile",
+    "students",
+    "teachers",
+    "employees",
+    "lessons",
+    "groups",
+    "subjects",
+    "rooms",
+  ],
+});
 
 export { constants };
