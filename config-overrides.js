0540e38006e2d93ed7dae8f090d99f23edac5348
jokurale <podsiadly.michal@protonmail.com>
2021-06-27 21:12:28 +0200

    Add 'rewireStyledComponents', edit overrides

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,12 @@
 const { alias, aliasJest, configPaths } = require("react-app-rewire-alias");
+const rewireStyledComponents = require("react-app-rewire-styled-components");
 
 const aliasMap = configPaths("./tsconfig.paths.json");
+
+module.exports = function override(config, env) {
+  config = rewireStyledComponents(config, env);
+  return config;
+};
 
 module.exports = alias(aliasMap);
 module.exports.jest = aliasJest(aliasMap);
