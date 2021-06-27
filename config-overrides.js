390509ad62ea672ac6afc741fe578cff816a89be
jokurale <podsiadly.michal@protonmail.com>
2021-06-27 21:17:21 +0200

    Remove latest changes v2, use built-in sc/macro instead of external solutions

--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,6 @@
 const { alias, aliasJest, configPaths } = require("react-app-rewire-alias");
-const rewireStyledComponents = require("react-app-rewire-styled-components");
 
 const aliasMap = configPaths("./tsconfig.paths.json");
-
-module.exports = function override(config, env) {
-  config = rewireStyledComponents(config, env);
-  return config;
-};
 
 module.exports = alias(aliasMap);
 module.exports.jest = aliasJest(aliasMap);
